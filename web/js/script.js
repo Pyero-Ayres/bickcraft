d7debad8942bb559bcf38112ca5ba3f9be727ce8
Pyero-Ayres <pyero.cultura@protonmail.com>
2021-01-28 19:31:47 -0300

    configuração da parte de php da pagina.

--- web/js/script.js
+++ web/js/script.js
@@ -11,3 +11,9 @@ if (window.SimpleSlide) {
 }
 
 new SimpleAnime();
+new SimpleForm({
+    form: ".formphp", //seletor do formulário
+    button: "#enviar", //seletor do botão
+    erro: "<div id= 'form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", //mensagem de erro
+    sucesso: "<div id= 'form-sucesso'><h2>Formulário enviado com sucesso.</h2><p>Em breve entraremos em contato com você.</p></div>", //mensagem de sucesso
+});
